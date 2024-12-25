c89310aef838192dc451543cece730f38ec9d930
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-12-25 13:25:36 +0200

    Updates

---
+++ assets/Store-BGvP8nBM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Dywl-Ta8.js";const r=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Welcome to the Store"}),e.jsx("p",{children:"This is a sample shop page."})]});export{r as default};
