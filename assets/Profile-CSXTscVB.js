1f873eff5d2043e65c07e6f6d6d79deca64e0236
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-12-31 08:21:21 +0200

    Updates

---
+++ assets/Profile-CSXTscVB.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-RyDAEoDZ.js";import"./AdvertForm-CtMT9_Aw.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Welcome to the Profile"})});export{t as default};
--- assets/Profile-CVH6aoK0.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-U7RwY6LR.js";import"./AdvertForm-DapVNliq.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Welcome to the Profile"})});export{t as default};
