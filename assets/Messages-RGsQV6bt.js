a2904591a02492591749bbc83e5a7bd42a57973d
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2025-01-13 16:43:21 +0200

    Updates

---
+++ assets/Messages-RGsQV6bt.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-P7OHD99y.js";const r=()=>s.jsx("div",{children:s.jsx("h2",{children:"У Вас повідомлень немає"})});export{r as default};
--- assets/Messages-x2C7gWx2.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BjmqZ9iP.js";const r=()=>s.jsx("div",{children:s.jsx("h2",{children:"У Вас повідомлень немає"})});export{r as default};
