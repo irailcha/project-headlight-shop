44005ae0e64599068b3d0a9aae57b9e1fe78e748
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2025-04-22 15:35:49 +0300

    змінено з HashRouter на BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/global.scss';
 import {store} from './redux/store.js';
@@ -11,9 +11,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
     <ErrorBoundary>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
 </ErrorBoundary>
     </Provider>
   </StrictMode>
